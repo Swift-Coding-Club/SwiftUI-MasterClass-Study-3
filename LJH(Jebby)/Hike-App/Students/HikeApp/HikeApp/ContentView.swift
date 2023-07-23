c5ce8e0998ad512ad0acee0da92351da9f7c0938
Jehyeon Lee <dasom8899@naver.com>
2023-07-24 03:36:14 +0900

    HikeApp
    Project Setup

---
+++ LJH(Jebby)/Hike-App/Students/HikeApp/HikeApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HikeApp
+//
+//  Created by LJh on 2023/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+        .resizable()
+        .scaledToFit()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
