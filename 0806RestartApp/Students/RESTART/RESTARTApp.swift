9046f91cd0554ec7f3315f755440fbb269847ee7
kempt007 <133959724+kempt007@users.noreply.github.com>
2023-08-06 17:38:27 +0900

    RESTART App
    두번째 앱 개발

---
+++ 0806RestartApp/Students/RESTART/RESTARTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESTARTApp.swift
+//  RESTART
+//
+//  Created by 윤경환 on 2023/08/06.
+//
+
+import SwiftUI
+
+@main
+struct RESTARTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
